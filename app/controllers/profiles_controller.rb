ccbe5027b52b026f3b1ebc09544d912d964b955a
Jory Schmidt <joryschmidt1988@gmail.com>
2015-06-03 23:09:29 +0000

    added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,21 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:user_id])
     @profile = @user.build_profile
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = 'Your profile is good to go, bruh'
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :new
+      flash[:danger] = 'There was some kind of problem. One of us has to be better at this.'
+    end
+  end
+  
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :phone_number, :contact_email, :description, :job_title)
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :contact_email, presence: true
 end
