2e496da238a9d63401da1600c0119de2115a07c6
Jory Schmidt <joryschmidt1988@gmail.com>
2015-06-05 00:16:08 +0000

    added page security to controllers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   def new
     # form for user to create own profile
     @user = User.find(params[:user_id])
@@ -16,13 +18,31 @@ class ProfilesController < ApplicationController
       flash[:danger] = 'There was some kind of problem. One of us has to be better at this.'
     end
   end
+  
   def edit
     @user = User.find(params[:user_id])
     @profile = @user.profile
   end
   
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:danger] = "So sorry, there's been an issue. Please try again."
+      render action: :edit
+    end
+  end
+  
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :phone_number, :contact_email, :description, :job_title)
+  end
+  
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to root_url unless @user == current_user
   end
 end
